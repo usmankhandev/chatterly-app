301f6aec20e3e5bd6a1eb809ffb5f346a0f33ac7
Osman Khan <osmankhan.dev@gmail.com>
2025-07-04 00:27:06 +0500

    Users feature has been implemented.

---
+++ server/src/features/users/user.service.ts
@@ -0,0 +1,30 @@
+import { userModel } from './user.model';
+
+export const UserService = {
+    createUser: async (data: {name: string, email: string, password: string}) => {
+        return userModel.create({data});
+    },
+
+    getAllUsers: async() => {
+        return userModel.findMany();
+    },
+
+    getUserById: async(id: string) => {
+        return userModel.findUnique({
+            where: {id}
+        });
+    },
+
+    deleteUser: async(id: string) => {
+        return userModel.delete({
+            where: {id}
+        });
+    },
+    
+    updateUser: async(id: string, data: {name?: string, email?: string, password?: string}) => {
+        return userModel.update({
+            where: {id},
+            data,
+        });
+    },
+}
