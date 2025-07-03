76dd8a6f78b2b164d25563e4db45c875794725d5
Osman Khan <osmankhan.dev@gmail.com>
2025-07-04 00:27:06 +0500

    Users feature has been implemented.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import userRouter from './routes/user.route';
+// src/index.ts
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+import dotenv from 'dotenv';
+import path from 'path';
+
+
+const resolvedEnvPath = process.env.NODE_ENV === 'production'
+  ? path.join(__dirname, '../.env')
+  : path.join(__dirname, '../.env.local');
+
+dotenv.config({path: resolvedEnvPath});
+
+console.log(`Loaded env: ${resolvedEnvPath}`);
+console.log(`loaded DB_URL: ${process.env.DATABASE_URL}`);
+
+
+// application entry point
+
+import app from './app';
+import prisma from './config/prismaClient';
+
+prisma.$connect()
+  .then(() => {
+    console.log('✅ Connected to the database successfully');
+  })
+  .catch((error) => {
+    console.error('❌ Failed to connect to the database:', error);
+    process.exit(1); // Exit the process if the database connection fails 
+  });
+
 
 const PORT = process.env.PORT || 3001;
 
-// routes;
-
-app.get('/', (req, res) => {
-  res.send('✅ Chatterly backend is up');
-});
-
-app.use('/users', userRouter);
-
-// listening to the server.
-
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on port ${PORT}`);
+  console.log(`🚀 Chatterly backend is live on http://localhost:${PORT}`);
 });
+
