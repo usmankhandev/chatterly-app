8cf34bbb8927d47e74c9bbc29a551adf52476092
Osman Khan <osmankhan.dev@gmail.com>
2026-02-24 20:45:02 +0500

    Sockets code refactored.

--- server/src/socket/socket.server.ts
+++ server/src/socket/socket.server.ts
@@ -1,7 +1,5 @@
 import { Server as HttpServer } from 'http';
 import { Server, Socket } from 'socket.io';
-import { Notification } from '@prisma/client';
-import { TokenUtils } from '../utils/shared/auth.utils';
 import { presenceService } from '../services/presence.service';
 
 interface ConnectedUser {
@@ -56,37 +54,6 @@ export class SocketServer {
             .emit('friend:offline', { userId, lastSeen: user.lastSeen });
         }
         this.removeConnectedUser(userId, socket.id);
-      });
-
-      // Example: Join post rooms
-
-      socket.on('join:post', (postId: string) => {
-        socket.join(`post: ${postId}`);
-        console.log(`User ${userId} joined post room: ${postId}`);
-      });
-
-      socket.on('leave:post', (postId: string) => {
-        socket.leave(`post: ${postId}`);
-        console.log(`User ${userId} left the post room: ${postId}`);
-      });
-
-      // Marking notification as read via socket
-      socket.on('notification:read', (notificationId: string) => {
-        console.log(`User ${userId} read notification: ${notificationId}`);
-        // Broadcast to all connections of this user
-        this.broadcastNotificationRead(userId, notificationId);
-      });
-
-      // Handle notification deletion
-      socket.on('notification:delete', (notificationId: string) => {
-        console.log(`User ${userId} deleted notification: ${notificationId}`);
-        this.broadcastNotificationDeleted(userId, notificationId);
-      });
-
-      // Fetch unread notifications count
-      socket.on('notification:get-unread-count', async () => {
-        // This will be handled by the notification service
-        console.log(`User ${userId} requested unread count`);
       });
     });
     console.log(`Socket.io server initialized`);
